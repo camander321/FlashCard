4243f700d480aec785f600ca9c4e3cbaab25594f
Cameron Anderson and John Rykken <john@>
2018-01-09 14:26:43 -0800

    Add stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $(".topic").click(function() {
     $(".definition").hide();
     $("#" + this.id + "-def").fadeIn();
+    console.log($("#clicked-topic").innerHTML);
+    document.getElementById("clicked-topic").innerHTML = this.innerHTML;
   });
+
 });
