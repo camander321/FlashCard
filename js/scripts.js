b9f85d4b9daab98a2344e39800c21146dda5ce8c
Cameron Anderson and John Rykken <john@>
2018-01-09 16:16:24 -0800

    add topic highlighting

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,20 @@ $(document).ready(function() {
     $(".definition").hide();
     $("#" + this.id + "-def").fadeIn();
     document.getElementById("clicked-topic").innerHTML = this.innerHTML;
+
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
   });
+
+  $("#highlight").click(function() {
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
+});
 
   $(".clear").click(function() {
     $(".definition").hide();
     document.getElementById("clicked-topic").innerHTML = "Click a Topic!"
+    $(".selected").removeClass("selected");
   });
 
 });
